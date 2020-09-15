48a7a655fb0936b84a912843014d038a85cbda02
Marjo Pantsar <s2000589@edu.bc.fi>
2020-09-15 11:54:10 +0300

    uusi projekti alkaa

---
+++ GymResults/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GymResults
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the beginning of a beautiful app");
+        }
+    }
+}
