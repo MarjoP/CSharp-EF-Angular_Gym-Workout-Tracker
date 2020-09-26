f7500d6e0a10311c8e3e6365f09e89fb1f563c69
Marjo Pantsar <s2000589@edu.bc.fi>
2020-09-26 09:23:07 +0300

    Lisää alasvetovalikot tulosten lisäys-sivulle ja hae olemassa olevat käyttäjät & lajit tietokannasta alasvetovalikkoihin.

--- GymWorkoutTracker/ClientApp/src/app/counter/counter.component.ts
+++ GymWorkoutTracker/ClientApp/src/app/counter/counter.component.ts
@@ -1,13 +1,55 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { WorkoutService } from '../workout.service';
 
 @Component({
   selector: 'app-counter-component',
-  templateUrl: './counter.component.html'
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss']
 })
-export class CounterComponent {
-  public currentCount = 0;
 
-  public incrementCounter() {
-    this.currentCount++;
+export class CounterComponent {
+  public users: any;
+  public exercises: any;
+
+  public selectedUser: string;
+  public selectedExercise: string;
+  public repeats: number;
+  public weight: number;
+
+  constructor(private router: Router, private woService: WorkoutService) { 
+
+    this.woService.getUsers().subscribe(result => {
+      this.users = result;
+    }, error => console.error(error));
+
+    this.woService.getExercises().subscribe(result => {
+      this.exercises = result;
+    }, error => console.error(error));
   }
+
+  selUser(filterVal: any) {
+    this.selectedUser = filterVal;
+    if (filterVal == "newUser") {
+     
+    }
+  }
+
+  selExercise(filterVal: any) {
+    this.selectedExercise = filterVal;
+  }
+
+  /*
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+      data: { name: this.name, animal: this.animal }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }   */
+
 }
