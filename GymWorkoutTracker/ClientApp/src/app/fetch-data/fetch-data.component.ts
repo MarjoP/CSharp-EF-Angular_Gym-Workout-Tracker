a3d9b36d54eb342b7d3fdddc7889196aee65f512
Marjo Pantsar <s2000589@edu.bc.fi>
2020-10-02 19:04:45 +0300

    Lisää max-tulosten kuvaaja.

--- GymWorkoutTracker/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ GymWorkoutTracker/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { WorkoutService } from '../workout.service';
 import { Router } from '@angular/router';
 import { extractStyleUrls } from '@angular/compiler/src/style_url_resolver';
+import { Chart } from 'chart.js';
+import { forEachChild } from 'typescript/lib/tsserverlibrary';
+import Swal from 'sweetalert2';
 
 
 @Component({
@@ -11,7 +14,17 @@ import { extractStyleUrls } from '@angular/compiler/src/style_url_resolver';
   styleUrls: ['./fetch-data.component.scss']
 })
 export class FetchDataComponent {
-  public results: any;
+  public results: any = {
+    Date: "",
+    User: {
+      UserName: ""
+    },
+    Exercise: {
+      ExerciseName: ""
+    },
+    Repeats: "",
+    Weight: "",
+  }
   public users: any;
   public exercises: any;
 
@@ -20,7 +33,14 @@ export class FetchDataComponent {
   public quantity: number = -1;
 
   public show: boolean;
-  public graphVisible: boolean;
+  public graphVisible: boolean = true;
+
+  public canvas: any = <HTMLCanvasElement>document.getElementById('myChart');
+  public chart: any;
+  public ctx: any;
+  public labels: String[] = [];
+  public MRes: String[] = [];
+
 
   constructor(private router: Router, private woService: WorkoutService) {
     this.woService.getResults().subscribe(result => {
@@ -48,30 +68,62 @@ export class FetchDataComponent {
   public fetchSorted() {
     this.woService.getSelectedResults(this.selectedUser, this.selectedExercise, this.quantity).subscribe(result => {
       this.results = result;
-      console.log(this.selectedUser);
-      console.log(this.selectedExercise);
-      console.log(this.quantity);
-      console.log(this.results);
-      this.show = true;
+      document.getElementById("res").style.display = "block";
+      document.getElementById("footer").style.display = "none";
     }, error => console.error(error));
   }
-  
-  public hideResults() {
-    this.show = false;
-  }
 
-  createGraph() {
-    this.getMax();
-    this.show = true;
-    this.graphVisible = true;
+  public hideResults() {
+    document.getElementById("res").style.display = "none";
+    document.getElementById("myChart").style.display = "none";
+    document.getElementById("footer").style.display = "block";
   }
 
   public getMax() {
     this.woService.getMaxResults(this.selectedUser, this.selectedExercise, this.quantity).subscribe(result => {
       this.results = result;
-      console.log(this.results);
-      this.show = true;
-    }, error => console.error(error));
+   
+      this.labels = this.results.map(function (e) {
+        return e.date.substr(0, 10);
+      });
+      this.MRes = this.results.map(function (e) {
+        return e.weight;
+      });
+
+      console.log(this.labels);
+      document.getElementById("footer").style.display = "none";
+      document.getElementById("res").style.display = "block";
+      document.getElementById("myChart").style.display = "block";
+
+      this.createGraph();
+    }, error => Swal.fire("Select one user and one exercise first!"));
   }
-  
+
+  createGraph() {
+    Chart.defaults.global.elements.line.tension = 0;
+    Chart.defaults.global.elements.line.borderColor= 'rgba(217, 0, 83, 0.5)';
+    this.canvas = <HTMLCanvasElement>document.getElementById('myChart');
+    const ctx = this.canvas.getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: this.labels,
+        datasets: [{
+          label: 'Max results',
+          data: this.MRes
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero:true
+            }
+          }]
+        }
+      }
+    });
+  }
+
+
 }
